5a007ae1aa4232eefc6998f7bd599f0a0d02a010
Rahul राहुल <rahul_yadav@external.mckinsey.com>
2023-05-03 10:38:52 +0530

    Array questions

--- src/main/java/Arrays/duplicatesArrayInPlace.java
+++ src/main/java/Arrays/duplicatesArrayInPlace.java
@@ -1,4 +1,22 @@
 package Arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class duplicatesArrayInPlace {
+    public static void findDuplicates(int [] arr){
+        Set<Integer> st = new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            if(st.contains(arr[i])){
+                System.out.println("Duplicate element found -----> "+arr[i]);
+            }else{
+                st.add(arr[i]);
+            }
+        }
+    }
+
+    public static void main(String [] args){
+        int [] arr = {3,2,4,5,3,4,7,9,1,0,2};
+        findDuplicates(arr);
+    }
 }
---
+++ src/main/java/Arrays/duplicatesWoLibrary.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class duplicatesWoLibrary {
+    public static int[] removeDuplicates(int[] arr) {
+        int[] uniqueArr = new int[arr.length];
+        int uniqueIndex = 0;
+        boolean isDuplicate;
+
+        for (int i = 0; i < arr.length; i++) {
+            isDuplicate = false;
+            for (int j = 0; j < uniqueIndex; j++) {
+                if (arr[i] == uniqueArr[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                uniqueArr[uniqueIndex] = arr[i];
+                uniqueIndex++;
+            }
+        }
+
+        int[] resultArr = new int[uniqueIndex];
+        System.arraycopy(uniqueArr, 0, resultArr, 0, uniqueIndex);
+        return resultArr;
+    }
+
+    public static void main(String [] args){
+        int[] arr = {1, 2, 3, 4, 5, 2, 3, 6, 7, 7, 8};
+        int[] uniqueArr = removeDuplicates(arr);
+        System.out.println(Arrays.toString(uniqueArr));
+    }
+}
