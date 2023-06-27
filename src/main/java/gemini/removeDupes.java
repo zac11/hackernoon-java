ddd12b6d9004bd325a48492a58c73e9d92d53e24
Rahul_Yadav <ydv.rahul09@gmail.com>
2023-06-27 10:03:53 +0530

    Update readme

--- src/main/java/gemini/removeDupes.java
+++ src/main/java/gemini/removeDupes.java
@@ -1,5 +1,8 @@
 package gemini;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class removeDupes {
 
     public static String removeDupesWoMap(String str){
@@ -15,9 +18,40 @@ public class removeDupes {
         return sb.toString();
     }
 
+    public static String returnDupeWords(String str){
+        String newString = "";
+        boolean [] seen = new boolean[256];
+
+        for(char c: str.toCharArray()){
+            if(!seen[c]){
+                newString+=c;
+                seen[c] = true;
+            }
+        }
+        return newString;
+    }
+
+    public static String returnDupeWords_Attempt2(String str){
+        Set<Character> hs = new HashSet<>();
+        String newString = "";
+
+        for(char c: str.toCharArray()){
+            if(!hs.contains(c)){
+                newString+=c;
+                hs.add(c);
+            }
+        }
+
+        return newString;
+    }
+
     public static void main(String [] args){
         String str = "my name is rahul yadav";
         String withoutrepeated = removeDupesWoMap(str);
+        String newwithoutrepeated = returnDupeWords(str);
+        String thirdAttempt = returnDupeWords_Attempt2(str);
         System.out.println("Without repeated---->"+withoutrepeated);
+        System.out.println("Without repeated second ---->"+newwithoutrepeated);
+        System.out.println("Without repeated third ---->"+thirdAttempt);
     }
 }
